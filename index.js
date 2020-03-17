dee0df1f4c33167b4ff26963409314dd51b3b009
max-student <max-student@maxtrain.com>
2020-03-17 16:26:59 -0400

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+var a = 0;
+var b = 1;
+function X() {
+    return 1;
+}
+var y = function () {
+    return 0;
+};
+var z = function () {
+    return -1;
+};
+var a1 = 5;
+function add1(n) {
+    return n + 4;
+}
+function x1(t) {
+    return;
+}
+var Customer = /** @class */ (function () {
+    function Customer(name, sales) {
+        if (name === void 0) { name = 'NEW Customer'; }
+        this.id = 0; //0 = default value
+        this.active = true;
+        this.name = name;
+        this.sales = 0;
+    }
+    Customer.prototype.print = function () {
+        console.log("" + this.name);
+    };
+    return Customer;
+}());
---
+++ index.ts
@@ -0,0 +1,36 @@
+let a = 0;
+let b = 1;
+function X(){
+    return 1;
+}
+let y = function(){
+    return 0;
+}
+const z = () => {
+    return -1;
+}
+
+let a1:number = 5;
+
+function add1(n: number) : number {
+    return n + 4;
+}
+
+function x1(t: number) : void {
+    return;
+}
+
+class Customer {
+    id: number = 0; //0 = default value
+    name: string;
+    active: boolean = true;
+    sales: number;
+
+    constructor(name:string = 'NEW Customer', sales:number){
+        this.name = name;
+        this.sales = 0;
+    } 
+    print() : void{
+        console.log(`${this.name}`);
+    }  
+}
